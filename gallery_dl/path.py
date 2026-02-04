92cf752465a22fcbcad3d32be07991c90971107f
CasualYT31 <21147925+CasualYT31@users.noreply.github.com>
2026-02-04 17:15:06 +0000

    Build paths into the metadata of extracted items

--- gallery_dl/path.py
+++ gallery_dl/path.py
@@ -307,6 +307,12 @@ class PathFormat():
         self.realpath = self.realdirectory + filename
         if not self.temppath:
             self.temppath = self.realpath
+        self.kwdict["_paths_"] = {
+            "filename": self.filename,
+            "path": self.path,
+            "realpath": self.realpath,
+            "temppath": self.temppath,
+        }
 
     def part_enable(self, part_directory=None):
         """Enable .part file usage"""
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from io import StringIO
 logs = StringIO()
 
 def on_metadata(metadata):
-    print(metadata["id"])
+    print(metadata["_paths_"])
 
 gallery_dl.main(["https://www.tiktok.com/@liverpoolfc", "--no-download", "-o", "tiktok-range=1-3"], logs, on_metadata)
 
